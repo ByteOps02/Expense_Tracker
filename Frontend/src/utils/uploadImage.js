e2ad02cb6fd89c2402f36bc71f344b9282fdd89f
Ram Krishna <Krishnarammhd@gmail.com>
2025-07-19 22:02:56 +0530

    Added new files in layouts and hooks in frontend

--- Frontend/src/utils/uploadImage.js
+++ Frontend/src/utils/uploadImage.js
@@ -1,21 +1,21 @@
-import { API_PATHS } from './apiPath';
-import axiosInstance from './axiosInstance';
+import { API_PATHS } from "./apiPath";
+import axiosInstance from "./axiosInstance";
 
 const uploadImage = async (imageFile) => {
-    const formData = new FormData();
-    // Append image file to form data
-    formData.append('image', imageFile);
+  const formData = new FormData();
+  // Append image file to form data
+  formData.append("image", imageFile);
 
-    const response = await axiosInstance.post(
-        API_PATHS.IMAGE.UPLOAD_IMAGE,
-        formData,
-        {
-            headers: {
-                'Content-Type': 'multipart/form-data', // Set header for file upload
-            },
-        }
-    );
-    return response.data;
+  const response = await axiosInstance.post(
+    API_PATHS.IMAGE.UPLOAD_IMAGE,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data", // Set header for file upload
+      },
+    }
+  );
+  return response.data;
 };
 
 export default uploadImage;
