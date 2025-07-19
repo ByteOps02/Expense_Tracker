4be9034b414c8b0199398fd4eb47708923d3b1fa
Ram Krishna <Krishnarammhd@gmail.com>
2025-07-20 00:10:57 +0530

    Fix the issue of ProfileImage in the SideMenu in the frontend

--- Frontend/src/pages/Dashboard/Home.jsx
+++ Frontend/src/pages/Dashboard/Home.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import { useUserAuth } from '../../../hooks/useUserAuth';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPath';
+import { UserContext } from '../../context/UserContext';
 
 const Home = () => {
   useUserAuth();
+
+  const [loading, setLoading] = useState(false);
+  const { updateUser } = useContext(UserContext);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+    setLoading(true);
+    try {
+      await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+      // No need to handle response since data is not displayed
+    } catch (error) {
+      console.log("Something went wrong. Please try again.", error);
+    }
+    setLoading(false);
+  };
+
+  const fetchUserInfo = async () => {
+    try {
+      const res = await axiosInstance.get(API_PATHS.AUTH.GET_USER_INFO);
+      if (res.data && res.data.user) {
+        updateUser(res.data.user);
+      }
+    } catch {
+      // Optionally handle error
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    fetchUserInfo();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className='my-5 mx-auto'></div>
+      <div className='my-5 mx-auto'>
+        {loading ? (
+          <div>Loading dashboard...</div>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </DashboardLayout>
-  )
+  );
 }
 
 export default Home
