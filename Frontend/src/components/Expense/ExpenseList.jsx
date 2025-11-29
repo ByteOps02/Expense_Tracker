3783b31d4a5990c42d09be59e91debadc4460c41
Ram Krishna <krishnarammhd@gmail.com>
2025-11-29 23:21:24 +0530

    made many changes in frontend as well as backend

--- Frontend/src/components/Expense/ExpenseList.jsx
+++ Frontend/src/components/Expense/ExpenseList.jsx
@@ -7,7 +7,7 @@ const ExpenseList = ({ transactions, onDelete, onDownload }) => {
   return (
     <div className="card">
       <div className="flex items-center justify-between">
-        <h5 className="text-lg">Expense Categories</h5>
+        <h5 className="text-lg font-semibold text-gray-900">Expense Categories</h5>
         <button className="card-btn" onClick={onDownload}>
           <LuDownload className="text-base" /> Download
         </button>
