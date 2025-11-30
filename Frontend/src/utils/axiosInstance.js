87ad434a8952b453962bb25a0d086b9093bdfe0e
Ram Krishna <krishnarammhd@gmail.com>
2025-11-30 23:49:01 +0530

    feat(frontend): Use VITE_BASE_URL from .env in axiosInstance

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./apiPath";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL || "http://localhost:5000", // fallback if BASE_URL is undefined
+  baseURL: BASE_URL,
   timeout: 10000,
   headers: {
     Accept: "application/json",
