8840ef885c8732df8313c351a3594e96d161dfd9
Ram Krishna <krishnarammhd@gmail.com>
2025-12-01 15:44:15 +0530

    Standardize padding values for cards, modals, and primary buttons.

--- Frontend/src/components/layouts/Modal.jsx
+++ Frontend/src/components/layouts/Modal.jsx
@@ -5,11 +5,11 @@ const Modal = ({ children, isOpen, onClose, title }) => {
 
   return (
     <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50">
-      <div className="relative p-4 w-full max-w-2xl max-h-full">
+      <div className="relative p-6 w-full max-w-2xl max-h-full">
         {/* Modal content */}
         <div className="relative bg-white rounded-lg shadow-lg">
           {/* Modal header */}
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+          <div className="flex items-center justify-between p-6 md:p-6 border-b rounded-t border-gray-200">
             <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
 
             <button
@@ -36,7 +36,7 @@ const Modal = ({ children, isOpen, onClose, title }) => {
           </div>
 
           {/* Modal body */}
-          <div className="p-4 md:p-5 space-y-4">{children}</div>
+          <div className="p-6 md:p-6 space-y-4">{children}</div>
         </div>
       </div>
     </div>
